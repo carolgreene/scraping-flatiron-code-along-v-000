2e333b935f6433f84da53e70b2132bb3c4d2225f
Carol Greene <carolgreene3@gmail.com>
2018-01-09 00:13:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,18 +31,17 @@ class Scraper
   end
 
 
-end
+    def print_courses
+        self.make_courses
+        Course.all.each do |course|
+          if course.title
+            puts "Title: #{course.title}"
+            puts "  Schedule: #{course.schedule}"
+            puts "  Description: #{course.description}"
+          end
+        end
+      end
 
-Scraper.new.get_page
+    end 
 
-
-    #def print_courses
-        #self.make_courses
-        #Course.all.each do |course|
-          #if course.title
-            #puts "Title: #{course.title}"
-            #puts "  Schedule: #{course.schedule}"
-            #puts "  Description: #{course.description}"
-          #end
-        #end
-      #end
+    Scraper.new.get_page
